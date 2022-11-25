ca379207cc42ea82644edb8b253a90d100df125c
kkrawiec <kkrawiec@sii.pl>
2022-11-25 15:33:51 +0100

    added reduce param

--- src/app/expenses/expense.service.ts
+++ src/app/expenses/expense.service.ts
@@ -4,26 +4,6 @@ import { Category, Expense } from "./expense.model";
 export class ExpenseService{
 
     private expenses: Expense[] = [
-        new Expense(
-          'Potatoes',
-          15,
-          Category.food
-        ),
-        new Expense(
-          'Ticket',
-          10,
-          Category.travels
-        ),
-        new Expense(
-          'Energy',
-          800,
-          Category.bills
-        ),
-        new Expense(
-          'Dentist',
-          2000,
-          Category.health
-        )
       ]
       
       expenseChanged = new BehaviorSubject<Expense[]>(this.expenses) // to zabieg aby pojawiały się składniki w tablicy "kopii"
@@ -37,10 +17,11 @@ export class ExpenseService{
         this.expenseChanged.next(this.expenses.slice()) // to zabieg aby pojawiały się składniki w tablicy "kopii"
       }
 
-      sumExpenses(category: Category){
+      sumExpenses(category: Category) {
+      
       return this.expenses
         .filter(function (el) { return el.category === category; })
         .map(function (el) { return el.price; })
-        .reduce(function (p, c) { return (p + c); });
+        .reduce(function (p, c) { return (p + c); }, 0);
       }
     }
